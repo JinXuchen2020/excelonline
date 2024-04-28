19fa113ced343e1584ff80caa73bc37e0a4ae8da
jinxuchen2020 <jinxuchen2020@icloud.com>
2024-04-28 10:35:02 +0800

    add source code

---
+++ ExcelOnline.Api/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using ExcelOnline.Api.Options;
+using ExcelOnline.Data.Models;
+
+namespace ExcelOnline.Api.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetUsers(UserQueryOption option);
+        Task<User?> GetUser(int id);
+
+        Task<User?> GetUserByPhone(string phoneNumber);
+
+        Task AddUser(User input);
+
+        Task<bool> ValidateUser(UserQueryOption option);
+    }
+}
---
+++ ExcelOnline.Data/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExcelOnline.Data.Models
+{
+    public class BaseModel
+    {
+        [ScaffoldColumn(false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
